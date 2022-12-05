69f576e1a2b9981b840fd16effd0a38d52a60eb0
김현욱 <company@wiselight.kr>
2022-12-05 21:58:41 +0900

    feat: 지갑 등록 명령어 구현 중 올바른 이더리움 지갑 주소인지 유효성 검사 #2

--- whitelist_bot.py
+++ whitelist_bot.py
@@ -1,20 +1,23 @@
 import time
+import re
 
 from selenium import webdriver
-from selenium.webdriver import ChromeOptions
+from selenium.webdriver import ChromeOptions, Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 SECONDBLOCK_ROOM_URL = 'https://2ndblock.com/room/yqDNWHh3xNLAsBO4mNEv'
 PROFILE_CSS_SELECTOR = '#game-screen > div:nth-child(2) > div.css-1ewmce8.ehzashr0 > div.css-azx95j > div.css-o16ypd > div.css-f4n6xs > span'
+ETHEREUM_ADDRESS_REGEX = r'^0x[a-fA-F0-9]{40}$'
 
 CHATTING_TAB_NICKNAME_SELECTOR = 'div.css-1o1z1uf'
 CHATTING_CONTENTS_TEXT_SELECTOR = 'div.css-rpu5yn.e112x67u4'
+CHATTING_INPUT_SELECTOR = 'input.chatting-input'
 
-LAST_CHAT_TEXT = {
+REGISTER_WALLET_COMMAND = '/지갑등록 '
 
-}
+LAST_CHAT_TEXT = {}
 
 
 def find_chat_elements():
@@ -39,6 +42,13 @@ def find_last_chat_text(chat_contents_texts):
     return chat_contents_texts[-1]
 
 
+def send_chatting(message):
+    chat_input = driver.find_element(By.CSS_SELECTOR, CHATTING_INPUT_SELECTOR)
+    chat_input.clear()
+    chat_input.send_keys(message)
+    chat_input.send_keys(Keys.RETURN)
+
+
 def handle_chat():
     chat_list_elements = find_chat_elements()
     for chat_element in chat_list_elements:
@@ -53,7 +63,13 @@ def handle_chat():
         last_chat_text = find_last_chat_text(chat_contents_texts)
 
         if nickname not in LAST_CHAT_TEXT or last_chat_text != LAST_CHAT_TEXT[nickname]:
-            print(last_chat_text)
+            if last_chat_text.startswith(REGISTER_WALLET_COMMAND):
+                arg = last_chat_text.replace(REGISTER_WALLET_COMMAND, '')
+                if re.match(ETHEREUM_ADDRESS_REGEX, arg):
+                    send_chatting('이더리움 지갑 주소 등록이 완료되었습니다.')
+                else:
+                    send_chatting('이더리움 지갑 주소를 정확하게 입력하셨는지 확인하세요.')
+
             LAST_CHAT_TEXT[nickname] = last_chat_text
 
     time.sleep(1)

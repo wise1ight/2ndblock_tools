b00f0be74969c44d5813697bb023d0aed2ec849e
김현욱 <company@wiselight.kr>
2022-12-05 21:03:47 +0900

    feat: 귓속말을 시도한 각 유저별 마지막 채팅 읽는 기능 구현 #2

--- whitelist_bot.py
+++ whitelist_bot.py
@@ -1,16 +1,51 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-SECONDBLOCK_ROOM_URL = 'https://2ndblock.com/room/kqlm15NawUT9X1a5vOQm'
+SECONDBLOCK_ROOM_URL = 'https://2ndblock.com/room/yqDNWHh3xNLAsBO4mNEv'
 PROFILE_CSS_SELECTOR = '#game-screen > div:nth-child(2) > div.css-1ewmce8.ehzashr0 > div.css-azx95j > div.css-o16ypd > div.css-f4n6xs > span'
 
-CHATTING_LIST_SELECTOR = 'div.css-n1fm3w'
 CHATTING_TAB_NICKNAME_SELECTOR = 'div.css-1o1z1uf'
 CHATTING_CONTENTS_TEXT_SELECTOR = 'div.css-rpu5yn.e112x67u4'
-CHATTING_ALL_CHAT_SELECTOR = 'div.todo-game-focus.Chatting_chat-user-scroll__GmCv_ > div:first-child'
+
+
+def find_chat_elements():
+    chat_list_elements = driver.find_elements(By.CSS_SELECTOR, CHATTING_TAB_NICKNAME_SELECTOR)
+    return chat_list_elements
+
+
+def select_chatting_tab(chat_element):
+    chat_element.click()
+
+
+def scan_chatting_text():
+    chat_contents_texts = driver.find_elements(By.CSS_SELECTOR, CHATTING_CONTENTS_TEXT_SELECTOR)
+    return list(map(lambda x: x.text, chat_contents_texts))
+
+
+def find_last_chat_text(chat_contents_texts):
+    return chat_contents_texts[-1]
+
+
+def handle_chat():
+    chat_list_elements = find_chat_elements()
+    for chat_element in chat_list_elements:
+        if chat_element.text == '전체 채팅':
+            continue
+
+        select_chatting_tab(chat_element)
+        chat_contents_texts = scan_chatting_text()
+        if len(chat_contents_texts) == 0:
+            continue
+        last_chat_text = find_last_chat_text(chat_contents_texts)
+
+        print(last_chat_text)
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -22,4 +57,6 @@ if __name__ == "__main__":
     WebDriverWait(driver, 3600).until(EC.presence_of_element_located((By.CSS_SELECTOR, PROFILE_CSS_SELECTOR)))
     print("로그인 감지")
 
+    while True:
+        handle_chat()
 
